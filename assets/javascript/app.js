007fee656b8dbd01c5dff6676bc531ddee74a61b
Brian Martin <Brian Martin>
2017-08-07 20:21:51 -0400

    javascript complete, broken link?

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,57 +1,63 @@
-var sports = ["NFL", "Baseball", "Denver Nuggets", "NHL"]
+$(document).ready(function(){
 
-function displaySportGifs() {
-	var sportVar = $(this).attr("data-name");
-	var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=b7c6a08723074e098dcafd64659b7c96&q=" + sportVar +"&limit=15&offset=0&rating=G&lang=en"
+	var sports = ["NBA", "Baseball", "Carolina Panthers", "NHL"];
 
-	$.ajax({
-		url: queryURL
-		method: "GET"
-	}).done(function(response){
+	function displaySportGifs() {
+
+		var sportVar = $(this).attr("data-name");
+		var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=b7c6a08723074e098dcafd64659b7c96&q=" + sportVar +"&limit=15&offset=0&rating=G&lang=en";
+
+		$.ajax({
+			url: queryURL
+			method: "GET"
+		}).done(function(response){
+			console.log(response);
+
+			var results = response.data;
+			for (var m = 0; m < results.length; m++) {
+				var sportDiv = $("<div>");
+				var p = $("<p>").text("Rated: " + results[i].rating)
+				var sportGif = $("<img>"); 
+
+				sportGif.attr("src", results[i].images.fixed_height.url);
+				sportDiv.append(p);
+				sportDiv.append(sportGif);
+
+				$("#gif-display").prepend(sportDiv);
+			}
+		}); //.done(function)
+	}; //displaySportGifs
+
+	function createButtons() {
+
+		//$("#button-display").empty();
+
+		for (var i = 0; i < sports.length; i++) {
+
+			var sb = $("<button>");
+
+			sb.addClass("selectedSport", "btn", "btn-info");
+			sb.attr("data-name", sports[i]);
+			sb.text(sports[i]);
+
+			$("button-display").append(sb);
+		};
+
+	}; //create buttons
+
+	$("#add-sport").on("click", function(event) {
+		event.preventDefault();
+
+		var f = $("#sport-input").val().trim();
 		
-		var sportDiv = $("<div class='sport'>");
-		var rating = response.rating;
-		var pOne = $("<p>").text("Rated: " + rating);
-		
-		sportDiv.append(pOne);
-		
-		var gifURL = response.images.fixed_height_small;
-		var gif = $("<img>").attr("src= " + gifURL);
-		
-		sportDiv.prepend(gif);
+		sports.push(f);
 
-		$("#gif-display").prepend(gif);
+		createButtons();
 
-	}) //.done(function)
-} //displaySportGifs
+	}); //click event
 
-function createButtons() {
+	$(document).on("click", ".selectedSport", displaySportGifs);
 
-	$("#button-display").empty();
+		createButtons();
 
-	for (var i = 0; i < sports.length; i++) {
-
-		var sportButton = $("<button>");
-
-		sportButton.addClass("selectedSport", "btn-info");
-		sportButton.attr("data-name", sports[i]);
-		sportButton.text(sports[i]);
-
-		$("button-display").append(sportButton);
-	}
-
-} //create buttons
-
-$("#add-sport").on("click", function(event) {
-	event.preventDefault();
-
-	var sportVar = $("#sport-input").val().trim();
-	sports.push(sportVar);
-
-	createButtons();
-
-}); //click event
-
-$(document).on("click", ".selectedSport", displaySportGifs);
-
-	createButtons();
+});//Document.ready
